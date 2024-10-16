22e15949fca58280b8ed418f951efc8e866ef865
Irina Fylyppova <irina.h.fylyppova@gmail.com>
2024-10-16 18:26:02 +0300

    content-worker added; some else lessons translated; ...a lot of other changes

--- load-fonts.js
+++ load-fonts.js
@@ -2,17 +2,17 @@ if ('fonts' in document) {
   Promise.all([
     document.fonts.load('14px Montserrat'),
     document.fonts.load('14px Luckiest Guy'),
-    document.fonts.load('16px Happy Monkey'),
+    document.fonts.load('14px Happy Monkey'),
     document.fonts.load('14px Poppins'),
-    document.fonts.load('13px Roboto')
+    document.fonts.load('14px Roboto')
   ]).then(() => {
-    document.documentElement.style.setProperty('--funy-font', 'Happy Monkey')
-    document.documentElement.style.setProperty('--main-font', 'Montserrat, Poppins')
-    document.documentElement.style.setProperty('--font-family', 'Montserrat, Poppins')
-    document.documentElement.style.setProperty('--lang-switcher-font', 'Luckiest Guy')
-    document.documentElement.style.setProperty('--welcome-win-font', 'Luckiest Guy')
-    document.documentElement.style.setProperty('--donate-button-font', 'Luckiest Guy')
-    document.documentElement.style.setProperty('--slogan-font', 'Happy Monkey')
+    document.documentElement.style.setProperty('--funy-font', 'Happy Monkey, Comic Sans MS, Textile, cursive')
+    document.documentElement.style.setProperty('--main-font', 'Montserrat, Poppins, Sans-Serif')
+    document.documentElement.style.setProperty('--font-family', 'Montserrat, Poppins, Sans-Serif')
+    document.documentElement.style.setProperty('--lang-switcher-font', 'Luckiest Guy, Poppins, Tahoma')
+    document.documentElement.style.setProperty('--welcome-win-font', 'Luckiest Guy, Poppins, Tahoma')
+    document.documentElement.style.setProperty('--donate-button-font', 'Luckiest Guy, Poppins, Tahoma')
+    document.documentElement.style.setProperty('--slogan-font', 'Happy Monkey, Comic Sans MS, Textile, cursive')
 
     document.documentElement.style.setProperty('--main-menu-width', '386px')
   })
